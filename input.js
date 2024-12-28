04f0a644961fa7df27c05f63429d005fcf991ec1
Vatanak <149386188+vatanak-leav@users.noreply.github.com>
2024-12-28 09:36:24 +0700

    Update input.js

--- input.js
+++ input.js
@@ -5,7 +5,7 @@ const displayText = document.getElementById('display_text');
 submitButton.addEventListener('click', () => {
     const inputValue = inputField.value; 
 
-    displayText.textContent = 'You entered :' + inputValue;
+    displayText.textContent = `You entered : ${inputValue}` ;
 
-    console.log('You entered :' + inputValue);
+    console.log(`You entered : ${inputValue}`);
 });
